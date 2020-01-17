25675a1441ef6b68a99ed0d88ed12953eb3c8aa4
tdieting <theresa.dietinger@dccs.at>
2020-01-17 07:55:17 +0100

    Updates

---
+++ precache-manifest.9779eff25e7cdc1da41957c765a68ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2193fa7137dcfe7770af6ed1af47b8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "66677262e1f616356825",
+    "url": "/portfolio/static/css/main.2fe34896.chunk.css"
+  },
+  {
+    "revision": "9b83b4d8832caa00af45",
+    "url": "/portfolio/static/js/2.413a256a.chunk.js"
+  },
+  {
+    "revision": "66677262e1f616356825",
+    "url": "/portfolio/static/js/main.5cb0d97e.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "ad7c82b60314ba716aa0cbe776467517",
+    "url": "/portfolio/static/media/blogging-coding-copywriting-34580.ad7c82b6.jpg"
+  }
+]);
